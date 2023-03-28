1d8f0429dd68b20d9042fe30a1127d553ce8390f
Like <like@ekil.sh>
2023-03-28 10:29:38 +0800

    .

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,12 @@
 const middleware = require('@blocklet/sdk/lib/middlewares');
 const router = require('express').Router();
 
-router.use('/user', middleware.user(), (req, res) => res.json(req.user || {}));
+router.use('/user', middleware.user(), (req, res) =>
+  res.json(
+    req.user || {
+      hello: 'world',
+    }
+  )
+);
 
 module.exports = router;
